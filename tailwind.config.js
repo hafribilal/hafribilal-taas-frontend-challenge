1a89428a84ea04efd8b52c8c3d0c6beb85728f75
Bilal Hafri <hafribilal@gmail.com>
2022-03-09 04:18:16 +0100

    setup youcan color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './src/components/*.{html,js,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'youcan': {
+          50: '#F8F8F8',
+          100: '#B09EBE',
+          200: '#A183AB',
+          300: '#946998',
+          400: '#844E7E',
+          500: '#70345E',
+          600: '#632D47',
+          700: '#562533',
+          800: '#481F21',
+          900: '#3A1D18',
+        },
+      }
+    },
   },
   plugins: [],
 }
