552bb342c2465de1585a8f36d80a3cef84e0b138
Bilal Hafri <hafribilal@gmail.com>
2022-03-09 05:02:30 +0100

    add guard for each of Auth page & Repos page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,23 +1,33 @@
+import { useUser } from '../store/user'
 
 const routes = [
   {
     path: "/",
     name: "repos",
     component: () => import('../components/Repositories.vue'),
-    // beforeEnter: (to, from, next) => {
-    //     try {
-    //         console.log('test');
-    //     } catch (err) {
-    //         console.error(err);
-    //         window.localStorage.clear();
-    //         next({ ame: 'auth' })
-    //     }
-    // },
+    beforeEnter: (to, from, next) => {
+      const user = useUser();
+      if (!user.isConnected) {
+        next({ name: 'auth' });
+        console.log('connect your gh account');
+      } else {
+        next();
+        console.log('welcome back');
+      }
+    },
   },
   {
     path: "/auth",
     name: "auth",
     component: () => import('../components/Auth.vue'),
+    beforeEnter: (to, from, next) => {
+      const user = useUser();
+      if (user.isConnected) {
+        next({ name: 'repos' })
+      } else {
+        next();
+      }
+    },
   },
   // {
   //   path: '*',
