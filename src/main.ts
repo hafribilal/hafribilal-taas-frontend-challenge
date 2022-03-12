f1c701ab0a6645ec2aa888bd78ef25816ae6f0c3
Bilal Hafri <hafribilal@gmail.com>
2022-03-13 00:01:19 +0100

    install and setup dayjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import App from './App.vue'
 import router from './router';
 import store from './store';
 
 import './style.css';
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App);
+
+dayjs.extend(relativeTime)
+
+app.use(router);
+app.use(store);
+app.config.globalProperties.$dayjs = dayjs;
+
+app.mount('#app');
