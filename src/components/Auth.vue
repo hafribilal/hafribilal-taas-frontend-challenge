7faf6ee5c30db45f514b7a3e70144a9ca1cee3f6
Bilal Hafri <hafribilal@gmail.com>
2022-03-18 05:14:07 +0100

    update the new Auth page to catch also the callbacks and also to match the new modifications

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,38 +1,60 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, defineProps, computed, watch, onMounted } from 'vue';
 import router from '../router'
 import { useUser } from '../store/user';
 import { default as TheAlert } from './TheAlert.vue';
 
-const isLoading = ref(false);
 const user = useUser();
-const authId = ref(user.authId);
+const isLoading = ref(false);
 const isAlertOpned = ref(false);
+const alertMsg = ref('');
+
+const props = defineProps<{query:any}>();
+
+const isCallback = computed(() => Boolean(props.query.code || props.query.error))
 
 async function connect(){
   isLoading.value = true;
+  // just a fake delay to user can see the loading interaction
   setTimeout(async()=> {
-    await user.connect();
-    if (user.isConnected) {
-      console.log('hello');
+    await user.connect()
+    .then(()=>{
       router.push({name:'repos'});
-    }else{
-      isAlertOpned.value = true;
-    }
-    isLoading.value = false;
+    })
+    .catch(err=>{
+      openAlert(err);
+    })
+    .finally(()=> isLoading.value = false);
   }, 300);
 }
+
+function openAlert(msg:string){
+  alertMsg.value = msg;
+  isAlertOpned.value = true;
+}
+function closePopup(){
+  setTimeout(()=>{
+    window.close();
+  },1500);
+}
+
+onMounted(()=>{
+  if (isCallback.value) {
+      closePopup();
+    }
+})
 </script>
 
 <template>
   <teleport to="main">
-    <TheAlert :show="isAlertOpned" :type="'error'" :duration="5000" @close="isAlertOpned = false">
-      <template #title><strong>Ops!! </strong> Connection Denied</template>
+    <TheAlert :show="isAlertOpned" :type="'error'" :duration="10000" @close="isAlertOpned = false">
+      <template #title><strong>Ops!! </strong> {{alertMsg}}</template>
     </TheAlert>
   </teleport>
   <section class="container">
     <button type="button"
             class="oauth-btn"
+            v-if="!isCallback"
             @click.prevent="connect()">
             <span v-if="!isLoading">
               Authorize my Github Account
@@ -45,10 +67,25 @@ async function connect(){
               Processing...
             </span>
     </button>
+    <div v-else class="">
+      <div v-if="props.query.code" class="success-checkmark">
+        <div class="check-icon">
+          <span class="icon-line line-tip"></span>
+          <span class="icon-line line-long"></span>
+          <div class="icon-circle"></div>
+          <div class="icon-fix"></div>
+        </div>
+      </div>
+      <div v-else class="">
+        <h1>Opps!!</h1>
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+@import '../assets/success-checkmark';
+
 .container{
   @apply m-0 w-screen h-screen flex flex-col flex-1 justify-center items-center p-6;
 }
